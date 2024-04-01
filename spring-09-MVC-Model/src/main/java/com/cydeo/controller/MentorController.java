e1b442a2bd82b8e93f6a4488b3e771ddb476d9b7
Matt Camly <mehmeteshan@gmail.com>
2024-03-31 22:51:34 -0500

    spring model-view-controller continue

---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,30 @@
+package com.cydeo.controller;
+
+import com.cydeo.enums.Gender;
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+
+public class MentorController {
+@RequestMapping("/list")
+    public String showTable(Model model){
+
+    List<Mentor> mentorList = new ArrayList<>();
+    mentorList.add(new Mentor("Mike","Smith",45, Gender.MALE));
+    mentorList.add(new Mentor("Matt","Hanks",35, Gender.MALE));
+    mentorList.add(new Mentor("Mary","Brtyan",42, Gender.FEMALE));
+
+    model.addAttribute("mentors",mentorList);
+
+    return "mentor/mentor-list";
+
+    }
+
+
+}
