4238461cc393e29651dd01a99334dbf468666ed3
Matt Camly <mehmeteshan@gmail.com>
2024-05-04 00:19:54 -0500

    spring data relationship

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -0,0 +1,40 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payments")
+@NoArgsConstructor
+@Data
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate createdDate;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private Status paymentStatus;
+
+    //@OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    private PaymentDetail paymentDetail;
+
+    @ManyToOne
+    private Merchant merchant;
+
+    public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
+        this.createdDate = createdDate;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
+}
